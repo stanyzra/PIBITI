aecaf09752a388f8789fdaea54443c1f0edb3235
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-02-14 15:07:08 -0300

    Ajuste no script de leitura e fusão de predicts.

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import scripts
 
 #rafael.comparar_labels("labels/labelSVM.txt", "labels/predict_LBP.txt")
 
-scripts.features_to_svm_train_test("features/mfcc50.txt", 80, 5, 3, 2)
+# scripts.features_to_svm_train_test("features/ssd.txt", 80, 5, 3, 2)
 
 # scripts.ler_predict("./libsvm-3.24/tools/teste.txt.predict", 2)
+
+# scripts.fusao("teste.txt.predict.lbp", "teste.txt.predict.ssd", 80)
+
+scripts.ler_predict("teste.txt.predict.lbp", 2)
+scripts.ler_predict("teste.txt.predict.ssd", 2)
+
+scripts.ler_predict("predictSoma.txt", 2)
+scripts.ler_predict("predictProduto.txt", 2)
+scripts.ler_predict("predictMax.txt", 2)
--- scripts.py
+++ scripts.py
@@ -2,6 +2,8 @@
 Arquivo que possui as funções para editar arquivos e rodar o SVM
 """
 
+import numpy as np
+
 '''
 @Descrição
 Função que recebe um arquivo "cru" de características por parâmetro e
@@ -22,6 +24,8 @@ c1 c2 c3 ... cn
 Classe 1:c1 2:c2 3:c3 ... n:cn
 '''
 def features_to_svm_train_test(arquivo_caract, num_classes, amostras_por_classe, qtde_treino, qtde_teste):
+
+    print("Iniciando a separação das amostras em treino e teste...")
 
     # Abre o arquivo, lê o conteúdo para um vetor e fecha
     arquivo = open(arquivo_caract, "r")
@@ -69,6 +73,7 @@ def features_to_svm_train_test(arquivo_caract, num_classes, amostras_por_classe,
 
         # Fim da separação das amostras...
     
+    print("Criando arquivo de treino com {} amostra(s) para cada uma das {} classe(s)".format(qtde_treino, num_classes))
     # Abre o arquivo de saída de treino
     arquivo_treino = open("treino.txt", "w")
 
@@ -98,6 +103,7 @@ def features_to_svm_train_test(arquivo_caract, num_classes, amostras_por_classe,
     # Fecha o arquivo
     arquivo_treino.close()
 
+    print("Criando arquivo de teste com {} amostra(s) para cada uma das {} classe(s)".format(qtde_treino, num_classes))
     # Abre o arquivo de saída de treino
     arquivo_teste = open("teste.txt", "w")
 
@@ -133,6 +139,8 @@ def features_to_svm_train_test(arquivo_caract, num_classes, amostras_por_classe,
 Ler um predict e contar quantas amostras estão corretas
 """
 def ler_predict(nome_arquivo, amostras_por_classe, classe_inicial=0):
+
+    print("Abrindo um predict para leitura...")
     
     # Abre e lê o arquivo
     arquivo = open(nome_arquivo, "r")
@@ -148,9 +156,14 @@ def ler_predict(nome_arquivo, amostras_por_classe, classe_inicial=0):
     # Contador de classe pra ver se acertou
     classe_atual = classe_inicial
 
-    # Percorre o arquivo a partir da linha 2 porque a primeira é a de rótulos
-    # E +1 no len porque tem uma linha em branco no final e o len não conta ela
-    for linha in range(1, len(conteudo)):
+    # Verifica se tem a linha das labels
+    if "labels" in conteudo[0]:
+        inicio = 1
+    else:
+        inicio = 0
+    
+    # Percorre as linhas do arquivo
+    for linha in range(inicio, len(conteudo)):
 
         # Verifica se a linha não está vazia
         if conteudo[linha].strip() == "" or conteudo[linha].strip() == "\n":
@@ -173,10 +186,84 @@ def ler_predict(nome_arquivo, amostras_por_classe, classe_inicial=0):
         if cont % amostras_por_classe == 0:
             classe_atual += 1
     
-    acuracia = float(acertos / cont)
+    acuracia = float(acertos / cont)*100
 
+    print("\nResultado do arquivo {}:".format(nome_arquivo))
     print("Total de amostras: {}".format(cont))
     print("Total de acertos: {}".format(acertos))
     print("Total de acertos: {}/{} equivalente a {:.3f}%".format(acertos, cont, acuracia))
+    print("---------")
 
 
+"""
+@Descrição
+Script que faz a fusão de 2 arquivos do tipo predict com matriz de probabilidades.
+A fusão já gera o predict com a classe certa no começo da linha.
+"""
+def fusao(arquivo1, arquivo2, num_classes):
+
+    print("Iniciando a fusão dos arquivos {} e {}...".format(arquivo1, arquivo2))
+
+    arquivo = open(arquivo1, "r")
+    predict1 = arquivo.readlines()
+    arquivo.close()
+
+    arquivo = open(arquivo2, "r")
+    predict2 = arquivo.readlines()
+    arquivo.close()
+
+    # Cria os vetores para armazenar a fusão
+    vetSoma = []
+    vetProduto = []
+    vetMax = []
+
+    print("Realizando a fusão e buscando a nova classe de cada amostra...")
+
+    # Percorre o arquivo pulando a primeira linha porque só possui as labels
+    for linha in range(1, len(predict1)):
+
+        # Gera o vetor com os predicts somente do 0 até o 79, porque 80 (classes) não conta
+        vetPredict1 = predict1[linha].replace('\n', '').split(" ")#[0:num_classes]
+        vetPredict2 = predict2[linha].replace('\n', '').split(" ")#[0:num_classes]
+
+
+        # Mostra a última posição do vetor
+        # print("Vet predict:", vetPredictLBP[-1])
+
+        # Faz a fusão
+        linhaSom = [float(x) + float(y) for x, y in zip(vetPredict1, vetPredict2)]
+        linhaPro = [float(x) * float(y) for x, y in zip(vetPredict1, vetPredict2)]
+        linhaMax = [x if float(x) >= float(y) else y for x, y in zip(vetPredict1, vetPredict2)]
+
+        # Encontra a posição do maior valor, mas desconsidara a primeira coluna porque é a classe (label)      
+        # Se o label começa em 0 deixe como está. Se começar em 1 faça a soma +1
+        linhaSom[0] = np.argmax(np.array(linhaSom[1:].copy()))  # + 1
+        linhaPro[0] = np.argmax(np.array(linhaPro[1:].copy()))  # + 1
+        linhaMax[0] = np.argmax(np.array(linhaMax[1:].copy()))  # + 1
+
+        # Adiciona a linha que foi fundida ao vetor com todos os valores
+        vetSoma.append(linhaSom)
+        vetProduto.append(linhaPro)
+        vetMax.append(linhaMax)
+
+    print("Fusão realizada. Criando arquivos com resultados...")
+
+    # Criar os arquivos do zero no modo escrita
+    arquivoSoma = open("predictSoma.txt", "w")
+    arquivoProduto = open("predictProduto.txt", "w")
+    arquivoMax = open("predictMax.txt", "w")
+
+    # Para cada linha, escreve no arquivo aberto
+    for lin in range(len(vetSoma)):
+        linhaSoma = str(vetSoma[lin]).replace("[", "").replace("]", "").replace(",", "")
+        linhaProduto = str(vetProduto[lin]).replace("[", "").replace("]", "").replace(",", "")
+        linhaMax = str(vetMax[lin]).replace("[", "").replace("]", "").replace(",", "")
+
+        arquivoSoma.write(linhaSoma.replace("'", "")+"\n")
+        arquivoProduto.write(linhaProduto.replace("'", "")+"\n")
+        arquivoMax.write(linhaMax.replace("'", "")+"\n")
+
+    # Fecha os arquivos gerados
+    arquivoSoma.close()
+    arquivoProduto.close()
+    arquivoMax.close()
