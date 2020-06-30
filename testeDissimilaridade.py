867550bd13960c8d5eeb76522c17be18859ad554
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-06-30 15:36:12 -0300

    Script para contar os acertos na dissimilaridade.

--- testeDissimilaridade.py
+++ testeDissimilaridade.py
@@ -8,27 +8,87 @@ fileDiss.close()
 
 
 linhas = len(conteudoDiss)
-p = 80 #passo
+p = 160 #passo
+pos_vetor = 0
 
 vetDiss = [] #vetor que separa as caracteristicas em colunas para facilitar as operações
 
 for i in range(linhas):
     np.array(vetDiss.append(conteudoDiss[i].split()))
 
+
 vetPos = []
 colunas = len(vetDiss[0])
 
 #print(np.argmax(vetDiss[2]))
 
-#for que percorre o vetDiss à um passo de 80
+# Dados para  calcular a acurácia
+acertos = 0
+total_amostras = 160
 
+# Para verificar se a amostra foi classificada corretamente, faz esse passo
+# para verificar qual a maior probabilidade de ser positivo
+# Posição do vetor que é ele - ele: 1, 162, 323, 484... +161
+for i in range(1, linhas, p):
+
+    # vetor que é ele - ele    
+    pos_vetor += 1
+    
+    # Encontrar a posição do vetor que deve ser o maior acerto positivo
+    if pos_vetor %2 == 0:
+        certo_pos = pos_vetor-1
+    else:
+        certo_pos = pos_vetor+1
+        
+    # Variáveis para encontrar onde está o maior acerto positivo
+    maior_pos = 0
+    maior_taxa = 0
+    
+    # Percorre os 160 vetores para saber qual é o maior acerto positivo
+    # Faz um for com base no intervalo
+    for j in range(i, i+160): #de 1 até 160, pois a linha 0 são as labels
+    
+        if j == pos_vetor:
+            pass
+    
+        if float(vetDiss[j][1]) > maior_taxa:
+            maior_taxa = float(vetDiss[j][1])
+            maior_pos = j
+            
+    print("Posição do maior {} com taxa {}".format(maior_pos, maior_taxa))
+            
+    if maior_pos == certo_pos:
+        acertos += 1
+            
+    print("Vetor atual {}, onde ver o vetor certo {}, Maior posição {}".format(pos_vetor, certo_pos, maior_pos))
+            
+    pos_vetor += 160
+    
+print("Acertos: {}/160 = {}%".format(acertos, (acertos/160)*100))
 """
 
-for i in range(0, linhas, p):
-    for j in range(1, 80): #de 1 até 80, pois a linha 0 são as labels
-        if vetDiss[i][1] > vetDiss[j][1]:
-            pos = j
-            print(pos)
+Vetores de características
+
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+1 2 3 4
+10 10 10 10
+11 11 11 11
+12 12 12 12
+13 13 13 13
+10 11 12 13
+20 20 20 20
+21 21 21 21
+22 22 22 22
+23 23 23 23
+20 21 22 23
+30 30 30 30
+31 31 31 31
+32 32 32 32
+33 33 33 33
+30 31 32 33
 
 for i in range(0, linhas, p):
     print(i)
